73389d60c931d5ea9c00e634c5a6ce6f3245e2ac
Nikhil Dongarwar <90019084+NikhilDongarwar@users.noreply.github.com>
2023-12-31 13:48:09 +0530

    Create Sorting_Selection.java

---
+++ AllSortingAlgorthms/Sorting_Selection.java
@@ -0,0 +1,42 @@
+
+public class Sorting_Selection 
+{
+    void print_array(int a[])
+    {
+         System.out.println("\narray has");
+        for(int i=0;i<a.length;i++)
+            System.out.print(a[i]+",");
+    }
+
+void SelectionSort(int a[])
+    {
+        for(int i=0;i<a.length-1;i++)//passes
+        {
+            int min=a[i];
+            int pos=i;
+            for(int j=i+1;j<a.length;j++)//sort
+                    {
+                        if(a[j]<min)
+                        {
+                            min=a[j];
+                            pos=j;
+                        }
+                    }
+            a[pos]=a[i];
+            a[i]=min;
+        }
+    }
+  public static void main(String args[])
+    {
+        int a[]={66,11,33,99,88,22,44,55,77};
+        int b[]={22,11,44,33,55,66,77,88,99};
+        Sorting_Selection obj=new Sorting_Selection();
+        obj.print_array(a);
+
+        obj.SelectionSort(a);
+
+        obj.print_array(a);
+        
+    }
+}
+
